eca7d4d0d037be945a2c0f5f8f557d52d1bcee55
Benjamin Golder <benjamin.j.golder@gmail.com>
2014-10-18 15:47:28 -0400

    added packages path for server

--- codekitchen/manage.py
+++ codekitchen/manage.py
@@ -2,6 +2,12 @@
 import os
 import sys
 
+extra_folders = [
+    '/afs/athena.mit.edu/dept/cron/project/codekitchen/web_scripts/django',
+]
+for folder in extra_folders:
+    sys.path.append(folder)
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "codekitchen.settings")
 
