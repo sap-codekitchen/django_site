3ae77bedf8037c2258f556e7243574007769cf2a
Benjamin Golder <benjamin.j.golder@gmail.com>
2014-11-09 18:12:10 -0500

    refining forms, adding topic model

--- codekitchen/codekitchen/shared_settings.py
+++ codekitchen/codekitchen/shared_settings.py
@@ -9,6 +9,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
+    'kitchen',
 )
 
 MIDDLEWARE_CLASSES = (
--- codekitchen/scripts/load_db_models.py
+++ codekitchen/scripts/load_db_models.py
@@ -1,7 +1,20 @@
 from .add_users import add_super, add_staff
+from django.utils.text import slugify
 from faker import Factory
 
+from kitchen.models import Topic
+
 fake = Factory.create()
+
+def create_topics():
+    from .topics import initial_topics
+    new_topics = []
+    for name in initial_topics:
+        slug = slugify(name)
+        topic = Topic(name=name, slug=slug)
+        topic.save()
+        new_topics.append(topic)
+    return new_topics
 
 def create_supers(n=3):
     super_emails = [fake.email() for i in range(n)]
@@ -16,4 +29,5 @@ def create_users(n=10):
 def run():
     supers = create_supers()
     users = create_supers()
+    topics = create_topics()
     print supers[0].email
